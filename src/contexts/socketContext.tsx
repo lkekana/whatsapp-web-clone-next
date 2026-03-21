52cf127c1d107c3aad1d095c22cf595da00991da
Lesedi Kekana <theoriginalles@gmail.com>
2026-03-21 16:03:46 +0200

    fixed blank screen lol

--- src/contexts/socketContext.tsx
+++ src/contexts/socketContext.tsx
@@ -24,7 +24,7 @@ const SocketProvider = ({ children }: { children: React.ReactNode }) => {
 
 	if (!socket) {
 		// return <div>Loading socket…</div>;
-		return null;
+		return <>{children}</>
 	}
 
 	return (
